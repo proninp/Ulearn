14a4ca1ed3a83233458578d4bd18fc352612f680
proninp <velninpapro@gmail.com>
2024-08-15 14:55:14 +0300

    Ulearn - Loops - Dragon curve Practice - [2024.08.15]

---
+++ CSharp Basics Part 1/03. Loops/Fractals/DragonFractalTask.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Fractals;
+
+internal static class DragonFractalTask
+{
+    public static void DrawDragonFractal(Pixels pixels, int iterationsCount, int seed)
+    {
+        Random random = new Random(123);
+        double x = 1, y = 0;
+        double angleFirst = 45 * Math.PI / 180;
+        double angleSecond = 135 * Math.PI / 180;
+        pixels.SetPixel(x, y);
+        for(int i = 0; i < iterationsCount; i++)
+        {
+            double nY;
+            double nX;
+            bool isFirst = random.Next(2) % 2 == 0;
+            nX = CalculateX(x, y, angleFirst, angleSecond, isFirst);
+            nY = CalculateY(x, y, angleFirst, angleSecond, isFirst);
+            (x, y) = (nX, nY);
+            pixels.SetPixel(x, y);
+        }
+    }
+
+    private static double CalculateX(double x, double y, double angleFirst, double angleSecond, bool isFirst)
+    {
+        if (isFirst)
+            return (x * Math.Cos(angleFirst) - y * Math.Sin(angleFirst)) / Math.Sqrt(2);
+        return (x * Math.Cos(angleSecond) - y * Math.Sin(angleSecond)) / Math.Sqrt(2) + 1;
+    }
+    
+    private static double CalculateY(double x, double y, double angleFirst, double angleSecond, bool isFirst)
+    {
+        if (isFirst)
+            return (x * Math.Sin(angleFirst) + y * Math.Cos(angleFirst)) / Math.Sqrt(2);
+        return (x * Math.Sin(angleSecond) + y * Math.Cos(angleSecond)) / Math.Sqrt(2);
+    }
+}
---
+++ CSharp Basics Part 1/03. Loops/Fractals/README.md
@@ -0,0 +1,14 @@
+﻿# [Dragon curve](https://ulearn.me/course/basicprogramming/Praktika_Dragon_curve__f9dd2a00-cc48-42d7-914a-813e3f9847e0)
+
+[Скачайте проект DragonFractal](https://api.ulearn.me/slides/BasicProgramming/f9dd2a00-cc48-42d7-914a-813e3f9847e0/exercise/student-zip/Fractals.zip)
+
+В этой задаче вам нужно будет нарисовать вот такую фигуру:
+
+![example](res/dragon.png)
+
+Вряд ли это пригодится вам в будущем, но зато красиво! :)
+Подробнее про этот фрактал можете почитать, например, [в википедии](http://en.wikipedia.org/wiki/Dragon_curve).
+Алгоритм построения фрактала читайте в комментариях в классе DragonFractalTask
+Кстати, похожим образом можно построить ещё множество фракталов, в частности, [фрактальный папоротник](https://en.wikipedia.org/wiki/Barnsley_fern).
+
+[Решение](DragonFractalTask.cs)
