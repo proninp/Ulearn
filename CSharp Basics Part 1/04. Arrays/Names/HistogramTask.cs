4f7136030b6a27986dcd807615c41cebada080b4
proninp <velninpapro@gmail.com>
2024-08-17 18:21:03 +0300

    Ulearn - Arrays - Histogram - [2024.08.17]

---
+++ CSharp Basics Part 1/04. Arrays/Names/HistogramTask.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+namespace Names;
+
+internal static class HistogramTask
+{
+    public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+    {
+        var xLabels = Enumerable.Range(1, 31).Select(x => x.ToString()).ToArray();
+        var heat = new double[xLabels.Length];
+        
+        foreach (var nameData in names)
+        {
+            if (nameData.Name == name && nameData.BirthDate.Day != 1)
+                heat[nameData.BirthDate.Day - 1]++;
+        }
+        
+        return new HistogramData(
+            $"Рождаемость людей с именем '{name}'", 
+            xLabels, 
+            heat);
+    }
+}
