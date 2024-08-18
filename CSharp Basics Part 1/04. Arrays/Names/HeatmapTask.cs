f18c8ec01dcab5e081303a80f77c3f71ad3691fc
proninp <velninpapro@gmail.com>
2024-08-18 16:43:25 +0300

    Ulearn - Arrays - Histogram 2 - [2024.08.18]

--- CSharp Basics Part 1/04. Arrays/Names/HeatmapTask.cs
+++ CSharp Basics Part 1/04. Arrays/Names/HeatmapTask.cs
@@ -1,15 +1,17 @@
-﻿namespace Names;
+﻿using System.Linq;
+
+namespace Names;
 
 internal static class HeatmapTask
 {
     public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
     {
-        return new HeatmapData(
-            "Пример карты интенсивностей",
-            new double[,] { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 4 }, { 4, 4, 4 } }, 
-            new[] { "a", "b", "c", "d" }, 
-            new[] { "X", "Y", "Z" });
+        var y = Enumerable.Range(1, 12).Select(x => x.ToString()).ToArray();
+        var x = Enumerable.Range(2, 30).Select(x => x.ToString()).ToArray();
+        var heat = new double[30, 12];
+        foreach (var name in names)
+            if (name.BirthDate.Day != 1)
+                heat[name.BirthDate.Day - 2, name.BirthDate.Month - 1]++;
+        return new HeatmapData("Пример карты интенсивностей", heat, x, y);
     }
-
-    
 }
