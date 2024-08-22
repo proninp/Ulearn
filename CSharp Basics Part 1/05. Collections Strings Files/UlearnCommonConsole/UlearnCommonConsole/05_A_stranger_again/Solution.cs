996ca84e8f34b4dd28d13512acacd95d4472ec39
proninp <velninpapro@gmail.com>
2024-08-22 13:18:36 +0300

    Ulearn - Collections, Strings, Files - Task 05 - [2024.08.22]

---
+++ CSharp Basics Part 1/05. Collections Strings Files/UlearnCommonConsole/UlearnCommonConsole/05_A_stranger_again/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace UlearnCommonConsole._05_A_stranger_again
+{
+    public class Solution
+    {
+        public static string ApplyCommands(string[] commands)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (string command in commands)
+            {
+                if (command.Substring(0, 3) == "pop")
+                {
+                    var removeCount = int.Parse(command.Substring(4));
+                    sb.Remove(sb.Length - removeCount, removeCount);
+                }
+                else
+                    sb.Append(command.Substring(5));
+            }
+            return sb.ToString();
+        }
+    }
+}
