fa51e36bad7c7c697be22d22177cb12e22511e4e
proninp <velninpapro@gmail.com>
2024-08-27 14:28:23 +0300

    Ulearn - Collections, Strings, Files - Texts Generation - Frequency Analysis Task - [2024.08.29]

---
+++ CSharp Basics Part 1/05. Collections Strings Files/TextAnalysis/FrequencyAnalysisTask.cs
@@ -0,0 +1,46 @@
+﻿namespace TextAnalysis;
+
+static class FrequencyAnalysisTask
+{
+    public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
+    {
+        var map = new Dictionary<string, Dictionary<string, int>>();
+        foreach (var sentence in text)
+        {
+            for (int i = 0; i < sentence.Count - 1; i++)
+            { 
+                var bKey = sentence[i];
+                var bValue = sentence[i + 1];
+                AddNewPairWithCheck(map, bKey, bValue);
+                if (i + 2 < sentence.Count)
+                {
+                    var tKey = $"{bKey} {bValue}";
+                    var tValue = sentence[i + 2];
+                    AddNewPairWithCheck(map, tKey, tValue);
+                }
+            }
+        }
+
+        return map.ToDictionary(
+            kvp => kvp.Key,
+            kvp => kvp.Value.OrderByDescending(x => x.Value)
+                    .ThenBy(x => x.Key, StringComparer.Ordinal)
+                    .FirstOrDefault().Key);
+    }
+
+    private static void AddNewPairWithCheck(Dictionary<string, Dictionary<string, int>> map, string key, string value)
+    {
+        if (!map.ContainsKey(key))
+        {
+            map.Add(key, new Dictionary<string, int> { { value, 1 } });
+            return;
+        }
+        var innerMap = map[key];
+        if (!innerMap.ContainsKey(value))
+        {
+            innerMap.Add(value, 1);
+            return;
+        }
+        innerMap[value]++;
+    }
+}
---
+++ CSharp Basics Part 1/05. Collections Strings Files/TextAnalysis/SentencesParserTask.cs
@@ -0,0 +1,23 @@
+﻿namespace TextAnalysis;
+
+static class SentencesParserTask
+{
+    public static List<List<string>> ParseSentences(string text)
+    {
+        char[] wordsSeparators = {
+            '^', '#', '$', '-', '—', '+',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            '=', '\t', '\n', '\r', ',', '…', '“',
+            '”', '<', '>', '‘', '*', ' ', '/', '\u00A0'};
+
+        return text.Split(new[] { '.', '!', '?', ';', ':', '(', ')' },
+            StringSplitOptions.RemoveEmptyEntries)
+            .Select(w => w
+            .Split(wordsSeparators, StringSplitOptions.RemoveEmptyEntries)
+            .Select(w => w.Trim().ToLower())
+            .Where(w => w.Length > 0)
+            .ToList())
+            .Where(e => e.Count > 0)
+            .ToList();
+    }
+}
