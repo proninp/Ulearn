de8bb59a8b727b7d3002933e82e40abeeebe21c9
proninp <velninpapro@gmail.com>
2024-09-09 13:49:27 +0300

    Ulearn - Testing - Testing Practice - [2024.09.09]

---
+++ CSharp Basics Part 1/06. Testing/TableParser/TestingPracticeTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableParser;
+public class TestingPracticeTests
+{
+    [TestCase("a", new[] { "a" })]
+    [TestCase("a b", new[] { "a", "b" })]
+    [TestCase("\"a\"", new[] { "a" })]
+    [TestCase("\"a 'b' 'c' d\"", new[] { "a 'b' 'c' d" })]
+    [TestCase("\"a 'b'  'c' d\"", new[] { "a 'b'  'c' d" })]
+    [TestCase("'\"1\" \"2\" \"3\"'", new[] { "\"1\" \"2\" \"3\"" })]
+    [TestCase("'\\'1\\' \\'2\\' \\'3\\''", new[] { "'1' '2' '3'" })]
+    [TestCase("\"1\"   \"2\"", new[] { "1", "2" })]
+    [TestCase("''", new[] { "" })]
+    [TestCase("", new string[0])]
+    [TestCase("' '", new[] { " " })]
+    [TestCase("' a'", new[] { " a" })]
+    [TestCase("a\"b", new[] { "a", "b" })]
+    [TestCase("\"b ", new[] { "b " })]
+    [TestCase("'a'b", new[] { "a", "b" })]
+    [TestCase("\"b c d e\"", new[] { "b c d e" })]
+    [TestCase("\"def", new[] { "def" })]
+    [TestCase("\"a \\\"c\\\"", new[] { "a \"c\"" })]
+    [TestCase("\"a\" \\", new[] { "a", "\\" })]
+    [TestCase("\"\\\\\"", new[] { "\\" })]
+    [TestCase(" \"a\" ", new[] { "a" })]
+    public static void RunTests(string input, string[] expectedOutput)
+    {
+        Test(input, expectedOutput);
+    }
+
+    private static void Test(string input, string[] expectedOutput)
+    {
+        throw new NotImplementedException();
+    }
+}
